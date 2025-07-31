664d54554484ad8f66bc03977a2a943b98ffa644
forkanimahdi <forkanimahdi@gmail.com>
2025-07-31 15:34:31 +0100

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $products = Product::all();
+        return Inertia::render("products/index" , [
+            "products" => $products
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $request->validate([
+            "name" => "required",
+            "price" => "required|max_digits:2",
+            "quantity" => "required",
+        ]);
+
+        Product::create([
+            "name" => $request->name,
+            "price" => $request->price,
+            "quantity" => $request->quantity,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ resources/js/pages/products/index.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+
+import AppLayout from '@/layouts/app-layout';
+import { Head, useForm, usePage } from '@inertiajs/react';
+
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger, } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import InputError from '@/components/input-error';
+import { Loader2 } from 'lucide-react';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
+const breadcrumbs = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+
+const ProductPage = () => {
+    const { products } = usePage().props
+    const { data, setData, post, processing, errors, reset } = useForm({
+        "name": "",
+        "price": 0,
+        "quantity": 0
+    })
+
+    console.log(products);
+
+    const submitProduct = () => {
+        post("product/store", {
+
+            onError: () => { alert("mochkila") },
+            onSuccess: () => { reset("name", "price", "quantity") },
+            onFinish: () => {
+                product_modal.click()
+            }
+        })
+    }
+
+
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Products" />
+
+            <div className=" flex items-center justify-between p-6">
+                <h1>Product</h1>
+
+
+
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button id='product_modal' variant="outline">Create Product</Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-md">
+                        <DialogHeader>
+                            <DialogTitle>Create new Product</DialogTitle>
+                            <DialogDescription>
+                                PLease  insert a valid data  to create  the new  product
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="space-y-6">
+                            <div className="grid flex-1 gap-2">
+                                <Label htmlFor="link" className="sr-only">
+                                    Product Name
+                                </Label>
+                                <Input
+                                    value={data.name}
+                                    onChange={(e) => setData("name", e.target.value)}
+                                    placeholder='Insert a valid Product Name'
+                                />
+                                <InputError message={errors.name} />
+                            </div>
+                            <div className="">
+                                <Label htmlFor="link" className="sr-only">
+                                    Product Price
+                                </Label>
+                                <Input
+                                    value={data.price}
+                                    onChange={(e) => setData("price", e.target.value)}
+                                    type='number'
+                                    min={0}
+                                    placeholder='Insert a valid Product Price'
+                                />
+                                <InputError message={errors.price} />
+                            </div>
+                            <div className="grid flex-1 gap-2">
+                                <Label htmlFor="link" className="sr-only">
+                                    Stock
+                                </Label>
+                                <Input
+                                    value={data.quantity}
+                                    onChange={(e) => setData("quantity", e.target.value)}
+                                    type='number'
+                                    min={0}
+
+                                    placeholder='Insert a valid Product Quantity'
+                                />
+                                <InputError message={errors.quantity} />
+                            </div>
+                        </div>
+                        <DialogFooter className="sm:justify-end">
+                            <DialogClose asChild>
+                                <Button type="button" variant="secondary">
+                                    Close
+                                </Button>
+                            </DialogClose>
+                            <Button onClick={submitProduct} type="button" variant="default">
+                                {processing ? <div className="flex gap-3"><Loader2 className='animate-spin' />  Submiting ... </div> : "Create"}
+                            </Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+
+            </div>
+
+
+
+
+            <div className="px-6">
+
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px] uppercase">Id</TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Price</TableHead>
+                            <TableHead className="text-right">Quantity</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                    {
+                        products?.map((p , i) =>
+                        <>
+                        <TableRow>
+                            <TableCell className="font-medium">{p.id}</TableCell>
+                            <TableCell>{p.name}</TableCell>
+                            <TableCell className="text-right">${p.price}</TableCell>
+                            <TableCell className="text-right">{p.quantity} </TableCell>
+                        </TableRow>
+                        </>
+                        
+                        )
+                    }
+                    </TableBody>
+                </Table>
+
+
+            </div>
+        </AppLayout>
+    );
+};
+
+export default ProductPage;
